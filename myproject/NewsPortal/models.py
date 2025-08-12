0b1c5066f2b1948b80c33fe8a7eaa2e547b9598e
Egor <Egor-Fivegor@yandex.ru>
2025-08-12 15:58:30 +0300

    /

---
+++ myproject/NewsPortal/models.py
@@ -0,0 +1,83 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        # Суммарный рейтинг всех статей автора умножается на 3
+        post_rating = self.post_set.aggregate(total_rating=models.Sum('rating'))['total_rating'] or 0
+        post_rating *= 3
+
+        # Суммарный рейтинг всех комментариев автора через пользователя
+        user = self.user  # Получаем пользователя, связанного с автором
+        comment_rating = user.comment_set.aggregate(total_rating=models.Sum('rating'))['total_rating'] or 0
+
+        # Суммарный рейтинг всех комментариев к статьям автора
+        posts = self.post_set.all()
+        comments_rating = 0
+        for post in posts:
+            comments_rating += post.comment_set.aggregate(total_rating=models.Sum('rating'))['total_rating'] or 0
+
+        # Обновление рейтинга автора
+        self.rating = post_rating + comment_rating + comments_rating
+        self.save()
+
+    def __str__(self):
+        return self.user.username
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_type = models.CharField(max_length=10, choices=[('article', 'Статья'), ('news', 'Новость')])
+    created_at = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.content[:124] + '...'
+
+    def __str__(self):
+        return self.title
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.post.title} - {self.category.name}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.title}"
